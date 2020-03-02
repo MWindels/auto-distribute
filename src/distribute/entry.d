137224f0f8a28382eb3158396fa03e6669485269
MWindels <michael.l.windels@gmail.com>
2020-03-01 17:20:22 -0800

    log entry type now only recognizes non-mutable member functions

--- src/distribute/entry.d
+++ src/distribute/entry.d
@@ -13,7 +13,7 @@ import distribute.utils;
 private:
 
 /**
- * Returns a string representing a member field for the Params union converted from a member function of another type.
+ * Returns a string representing a member field for the Params union converted from a mutable member function of another type.
  * Used in conjunction with forAllMembers at compile time.
  *
  * Returns: A field declaration derived from the symbol.
@@ -21,25 +21,33 @@ private:
 static string _createParamField(alias symbol)(immutable string) pure @safe
 if(isFunction!symbol || isDelegate!symbol)
 {
-	//Start by defining a new parameter type tuple.
-	string decl = "Tuple!(";
-	
-	//Add the members of the parameter tuple.
-	//This is necessary because Parameters!symbol will return storage classes (which we don't want).
-	//We also make every member immutable, as an Entry is immutable to begin with.
-	alias pTypes = Parameters!symbol;
-	static foreach(immutable idx; 0 .. pTypes.length)
+	//Only return a new field declaration if the symbol is mutable (or non-const inout).
+	static if(!is(ConstOf!(typeof(symbol)) == typeof(symbol)) && !is(ImmutableOf!(typeof(symbol)) == typeof(symbol)))
 	{
-		decl ~= "immutable " ~ pTypes[idx].stringof;
-		static if(idx + 1 < pTypes.length)
+		//Start by defining a new parameter type tuple.
+		string decl = "Tuple!(";
+		
+		//Add the members of the parameter tuple.
+		//This is necessary because Parameters!symbol will return storage classes (which we don't want).
+		//We also make every member immutable, as an Entry is immutable to begin with.
+		alias pTypes = Parameters!symbol;
+		static foreach(immutable idx; 0 .. pTypes.length)
 		{
-			decl ~= ", ";
+			decl ~= "immutable " ~ pTypes[idx].stringof;
+			static if(idx + 1 < pTypes.length)
+			{
+				decl ~= ", ";
+			}
 		}
+		
+		//Complete the type and add the field name.
+		decl ~= ") " ~ ConsistentMangle!symbol ~ ";";
+		return decl;
 	}
-	
-	//Complete the type and add the field name.
-	decl ~= ") " ~ ConsistentMangle!symbol ~ ";";
-	return decl;
+	else
+	{
+		return "";
+	}
 }
 
 
@@ -55,10 +63,12 @@ immutable struct Entry(T)
 	 */
 	immutable union Params
 	{
-		Tuple!() get;
-		Tuple!(immutable T) set;
-		
 		mixin(forAllMembers!(T, _createParamField, nop, nop)());
+		
+		static if(__traits(allMembers, Params).length == 0)
+		{
+			Tuple!(immutable T) set;
+		}
 	}
 	
 	string[] node;
@@ -66,7 +76,7 @@ immutable struct Entry(T)
 	Params arguments;
 	
 	/**
-	 * Initializes an Entry using symbol, a member function of T.
+	 * Initializes an Entry using symbol, a mutable member function of T.
 	 */
 	this(alias symbol)(immutable string[] n, immutable Parameters!symbol args)
 	if(isFunction!symbol || isDelegate!symbol)
@@ -76,26 +86,18 @@ immutable struct Entry(T)
 		mixin("arguments." ~ ConsistentMangle!symbol) = Tuple!(mixin("arguments." ~ ConsistentMangle!symbol ~ ".Types"))(args);
 	}
 	
-	/**
-	 * Initializes an Entry for the get command.
-	 */
-	this(string name)(immutable string[] n)
-	if(name == "get")
+	static if(__traits(hasMember, Params, "set"))
 	{
-		node = n;
-		operation = name;
-		arguments.get = Tuple!()();
-	}
-	
-	/**
-	 * Initializes an Entry for the set command.
-	 */
-	this(string name)(immutable string[] n, immutable T arg)
-	if(name == "set")
-	{
-		node = n;
-		operation = name;
-		arguments.set = Tuple!(immutable T)(arg);
+		/**
+		 * Initializes an Entry for the set command.
+		 */
+		this(string name)(immutable string[] n, immutable T arg)
+		if(name == "set")
+		{
+			node = n;
+			operation = name;
+			arguments.set = Tuple!(immutable T)(arg);
+		}
 	}
 }
 unittest
@@ -103,37 +105,34 @@ unittest
 	class Example
 	{
 		void a() {}
-		float b(int x) {return 1.0;}
+		float b(int x) inout {return 1.0;}
 		ref string c(ref string x, char y, char z) {return x;}
+		int d(bool x, string y) const {return 1;}
+		@(1, "yes") void e() immutable {}
 	}
 	
-	void testGet() {}
-	void testSet(Example) {}
+	void testSet(T)(T) {}
 	
 	void test(T, alias symbol)(Parameters!symbol args)
 	if(isFunction!symbol || isDelegate!symbol)
 	{
 		immutable string[] node;
 		
-		static if(symbol.mangleof == testGet.mangleof)
+		static if(symbol.mangleof == (testSet!T).mangleof)
 		{
-			Entry!T e = Entry!T().__ctor!"get"(node);
-			assert(e.operation == "get");
-			assert(e.arguments.get.length == 0);
-		}
-		else static if(symbol.mangleof == testSet.mangleof)
-		{
-			immutable Example ex = new immutable(Example);	//Will need to come up with a scheme to deep-copy arbitrary data structures.
-			Entry!T e = Entry!T().__ctor!"set"(node, ex);
+			immutable T iArg = cast(immutable)args;	//May need to come up with a scheme to deep-copy arbitrary data structures.
+			Entry!T e = Entry!T().__ctor!"set"(node, iArg);
 			assert(e.operation == "set");
-			assert(tuple(ex) == e.arguments.set);
+			assert(tuple(iArg) == e.arguments.set);
+			assert(__traits(allMembers, Entry!T.Params).length == 1);
 		}
 		else
 		{
 			immutable Parameters!symbol iArgs = args;
 			Entry!T e = Entry!T().__ctor!symbol(node, iArgs);
 			assert(e.operation == ConsistentMangle!symbol);
-			assert(tuple(args) == mixin("e.arguments." ~ ConsistentMangle!symbol));	//Normally, one would use a switch statement on e.operation rather than a mixin like this.
+			assert(tuple(args) == mixin("e.arguments." ~ ConsistentMangle!symbol));	//At run time, one should use a switch statement on e.operation rather than a mixin like this.
+			assert(!__traits(compiles, Entry!T().__ctor!"set"));
 		}
 	}
 	
@@ -142,8 +141,9 @@ unittest
 	test!(Example, Example.a)();
 	test!(Example, Example.b)(1);
 	test!(Example, Example.c)(s, cast(char)1, cast(char)-1);
-	test!(Example, testGet)();
-	test!(Example, testSet)(new Example);
+	assert(!__traits(compiles, test!(Example, Example.d)));
+	assert(!__traits(compiles, test!(Example, Example.e)));
+	test!(int, testSet!int)(8);
 }
 
 void main()
